db85c0483ede57f8f558be372e84b1e21f83aaf2
Nathan Walters <nwalters512@gmail.com>
2015-01-07 11:57:22 -0600

    Removed trailing slash

--- android/src/main/java/org/wildstang/wildrank/android/utils/Constants.java
+++ android/src/main/java/org/wildstang/wildrank/android/utils/Constants.java
@@ -2,6 +2,6 @@ package org.wildstang.wildrank.android.utils;
 
 public abstract class Constants {
 
-	public static final String USB_FLASH_DRIVE_ROOT_PATH = "/storage/usbdisk0/";
+	public static final String USB_FLASH_DRIVE_ROOT_PATH = "/storage/usbdisk0";
     
 }
